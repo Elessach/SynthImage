d968236ab33b943f5a409636973947cffb7a1e89
padawan <padawan@debianUPEM-2016>
2018-02-09 13:22:29 +0100

    first commit

---
+++ tp1/minimal.c
@@ -0,0 +1,131 @@
+#include <SDL/SDL.h> // inclut la librairie SDL
+#include <GL/gl.h> // inclut la librairie open GL
+#include <GL/glu.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Dimensions de la fenêtre */
+static unsigned int WINDOW_WIDTH = 400;
+static unsigned int WINDOW_HEIGHT = 400;
+
+/* Nombre de bits par pixel de la fenêtre */
+static const unsigned int BIT_PER_PIXEL = 32;
+
+/* Nombre minimal de millisecondes separant le rendu de deux images */
+static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
+
+void resize(){
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-1., 1., -1., 1.);
+}
+
+int main(int argc, char** argv) {
+
+	
+    /* Initialisation de la SDL */
+    if(-1 == SDL_Init(SDL_INIT_VIDEO)) {
+        fprintf(stderr, "Impossible d'initialiser la SDL. Fin du programme.\n");
+        return EXIT_FAILURE;
+    }
+    
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,0);
+
+
+    /* Ouverture d'une fenêtre et création d'un contexte OpenGL */
+    if(NULL == SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE )) {
+        fprintf(stderr, "Impossible d'ouvrir la fenetre. Fin du programme.\n");
+        return EXIT_FAILURE;
+    }
+    
+    /* Titre de la fenêtre */
+    SDL_WM_SetCaption("Louise & Léa sont 2.", NULL);
+
+    //Nettoyer la fenetre
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    /* Boucle d'affichage */
+    int loop = 1;
+    while(loop) {
+
+        /* Récupération du temps au début de la boucle */
+        Uint32 startTime = SDL_GetTicks();
+        
+        /* Placer ici le code de dessin */
+
+        
+        /* Echange du front et du back buffer : mise à jour de la fenêtre */
+        SDL_GL_SwapBuffers();
+        
+        /* Boucle traitant les evenements */
+        SDL_Event e;
+        while(SDL_PollEvent(&e)) {
+
+            /* L'utilisateur ferme la fenêtre : */
+            if(e.type == SDL_QUIT) {
+                loop = 0;
+                break;
+            }
+            
+            /* Quelques exemples de traitement d'evenements : */
+            switch(e.type) {
+
+                /* Clic souris */
+                case SDL_MOUSEBUTTONUP:       
+                	glColor3ub(255,255,255);
+       				glVertex2f(-1 + 2. * e.button.x / WINDOW_WIDTH, -(-1 + 2. *e.button.y / WINDOW_HEIGHT));
+        			glEnd();
+                    break;
+
+                /* Touche clavier */
+                case SDL_KEYDOWN:
+                    printf("touche pressée (code = %d)\n", e.key.keysym.sym);
+                    switch(e.key.keysym.sym){
+                    	case 113:
+                    		loop = 0;
+                    		break;
+                    	case 112:
+                    		glBegin(GL_POINTS);
+                    		break;
+                    	case 108:
+							glBegin(GL_LINES);
+                    		break;
+                    	case 116:
+                    		glBegin(GL_TRIANGLES);
+                    		break;
+                    	default:
+                    		break;
+                    }
+                    break;
+
+                case SDL_MOUSEMOTION:
+                    glClearColor(((e.button.x)%255)/100, ((e.button.y)%255)/100, 0, 1);
+               	 	break;
+
+                case SDL_VIDEORESIZE:
+	                WINDOW_WIDTH = e.resize.w;
+	                WINDOW_HEIGHT = e.resize.h;
+                	resize();
+                	break;
+
+                default:
+                    break;
+            }
+        }
+
+        /* Calcul du temps écoulé */
+        Uint32 elapsedTime = SDL_GetTicks() - startTime;
+
+        /* Si trop peu de temps s'est écoulé, on met en pause le programme */
+        if(elapsedTime < FRAMERATE_MILLISECONDS) {
+            SDL_Delay(FRAMERATE_MILLISECONDS - elapsedTime);
+        }
+    }
+
+    /* Liberation des ressources associées à la SDL */ 
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
+
